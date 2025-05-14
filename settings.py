49f16a696ba8b9055a3e31cde6996fbc18705061
maria_zhiganova <my.nik.mariann@gmail.com>
2025-05-14 19:25:52 +0300

    adding js scripts

---
+++ settings.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+CSS_PATH = BASE_DIR.joinpath('static/css/bootstrap.min.css')
+JS_PATH = BASE_DIR.joinpath('static/js/bootstrap.bundle.min.js')
+IMG1_PATH = BASE_DIR.joinpath('static/images/sonyasha.jpg')
+MY_STYLES_PATH = BASE_DIR.joinpath('static/css/style.css')
